e8128b83707d205222e9cca48b6e5b75e80291c6
onpprod <otavionpproducoes@gmail.com>
2023-10-05 17:06:46 -0400

    Find in progess

--- sqldatabase.py
+++ sqldatabase.py
@@ -7,6 +7,7 @@ class SQLiteDB:
     """
     SQLITE by ONPPROD
     """
+
     def __init__(self, db_name):
         """
         :param db_name: path for the .db archive
@@ -25,16 +26,17 @@ class SQLiteDB:
         This function is not active
         :return: None
         """
-        pass
+        return
 
     def database(self):
         """
         This function is not activate
         :return: None
         """
-        pass
+        return
 
-    def create_index(self, index_columns, order, unique=True):
+    # ==================================================================================================================
+    def create_index(self, index_columns, order=None, unique=True):
         table_name = "students"
         if order is not None:
             print("Order is not used in SQLite")
@@ -67,103 +69,247 @@ class SQLiteDB:
         cursor.execute(query)
         self.conn.commit()
 
+    # ==================================================================================================================
     def insert_one(self, data):
-        print(data)
         table_name = 'variable_history'
-        timest = data['timestamp']
-        print(type(timest))
-        data['timestamp'] = self.convert_timestamp(timest)
+        timestamp = data['timestamp']
+        data['timestamp'] = self.convert_timestamp(timestamp)
         data = self.convert_data_to_string(data)
-        cursor = self.conn.cursor()
-
-        placeholders = ', '.join(['?'] * len(data))
-        columns = ', '.join(data.keys())
-        values = tuple(data.values())
-
-        query = f"""
-            INSERT INTO {table_name} ({columns})
-            VALUES ({placeholders});
-        """
-
-        tables = self.get_tables()
-        if table_name not in tables:
-            create_query = ''
-            for column in data.keys():
-                create_query += column + ","
-            self.create_table(table_name, create_query[:-1])
-
-        cursor.execute(query, values)
-        self.conn.commit()
+        self.insert_dict(table_name, data)
 
     async def insert_one_by_collection(self, table_name, data):
         data['timestamp'] = self.convert_timestamp(data['timestamp'])
         data = self.convert_data_to_string(data)
-        cursor = self.conn.cursor()
-        placeholders = ', '.join(['?'] * len(data))
-        columns = ', '.join(data.keys())
-        values = tuple(data.values())
-        query = f"""
-            INSERT INTO {table_name} ({columns})
-            VALUES ({placeholders});
-        """
-        await cursor.execute(query, values)
-        await self.conn.commit()
+        self.insert_dict(table_name, data)
 
-    async def find(self, query, projection=None, size=1000):
-        cursor = self.conn.cursor()
-        # Construa a consulta SQL com base nos critérios de pesquisa
-        query_sql = "SELECT * FROM variable_history WHERE "
+    # ==================================================================================================================
+    # async \
+    def find(self, query, projection=None, size=1000):
+        table_name = "variable_history"
+        columns = self.get_columns(table_name)
+        query_sql, values = self.find_query_generator(table_name,columns,query,None,size)
+
+        new_values = []
+        for value in values:
+            if isinstance(value, dict):
+                new_values.append(self.convert_timestamp(value))
+            else:
+                new_values.append(value)
+        values = None
+
+        self.cursor.execute(query_sql, new_values)
+        results = self.cursor.fetchall()
+
+        # if projection:
+        #     if 'value' in projection and projection['value'] == 1:
+        #         print()
+        #     if '_id' in projection and projection['_id'] == 0:
+        #         print()
+        output = []
+        for document in results:
+            output.append(self.convert_data_to_dict(self.convert_data_to_zip(columns, document)))
+        return output
+
+    async def find_by_collection(self, collection: str, query: dict, projection=None, size=1000, sort=None):
+        table_name = "variable_history"
+        columns = self.get_columns(table_name)
+        query_sql, values = self.find_query_generator(table_name, columns, query, None,size)
+        self.cursor.execute(query_sql, values)
+        results = self.cursor.fetchall()
+        # if projection:
+        #     if 'value' in projection and projection['value'] == 1:
+        #         print()
+        #     if '_id' in projection and projection['_id'] == 0:
+        #         print()
+        output = []
+        for document in results:
+            output.append(self.convert_data_to_dict(self.convert_data_to_zip(columns, document)))
+        return output
+
+        # if sort is None:
+        #     sort = []
+        #
+        # if projection is None:
+        #     projection = {}
+        #
+        # db = self.database()
+        # cursor = db[collection].find(query, projection).limit(size).sort(sort)
+        #
+        # return await cursor.to_list(size)
+
+
+    # ==================================================================================================================
+    def count(self):
+        nome_tabela = "variable_history"
+        consulta_sql = f"SELECT COUNT(*) FROM {nome_tabela}"
+        self.cursor.execute(consulta_sql)
+        resultado = self.cursor.fetchone()[0]
+        return resultado
+
+    def count_by_collection(self, nome_tabela):
+        consulta_sql = f"SELECT COUNT(*) FROM {nome_tabela}"
+        self.cursor.execute(consulta_sql)
+        resultado = self.cursor.fetchone()[0]
+        return resultado
+
+    # ==================================================================================================================
+    """
+    Auxiliary data manipulation and conversion functions
+    """
+    # ==================================================================================================================
+
+    def find_query_generator(self,table_name,columns,query,sort,size):
+        where = "WHERE" if len(query) > 0 else ""
+        query_sql = f"SELECT * FROM {table_name} {where} "
         conditions = []
         values = []
-
         if '$and' in query:
             for condition in query['$and']:
                 for key, subquery in condition.items():
                     if key == 'timestamp':
                         if '$gte' in subquery:
                             conditions.append('timestamp >= ?')
-                            values.append(subquery['$gte'])
-                        if '$lte' in subquery:
+                            if isinstance(subquery['$gte'], dict):
+                                timestamp_unix = self.convert_timestamp(subquery['$gte'])
+                            else:
+                                date_str = subquery['$gte'].replace('Z', '+00:00')
+                                datetime_obj = datetime.fromisoformat(date_str)
+                                timestamp_unix = int(datetime_obj.timestamp())
+                            values.append(timestamp_unix)
+                        if 'c' in subquery:
                             conditions.append('timestamp <= ?')
-                            values.append(subquery['$lte'])
+                            if isinstance(subquery['$lte'], dict):
+                                timestamp_unix = self.convert_timestamp(subquery['$gte'])
+                            else:
+                                date_str = subquery['$lte'].replace('Z', '+00:00')
+                                datetime_obj = datetime.fromisoformat(date_str)
+                                timestamp_unix = int(datetime_obj.timestamp())
+                            values.append(timestamp_unix)
                     elif key == 'idShort':
                         conditions.append('idShort = ?')
                         values.append(subquery)
 
+        for column in columns:
+            if column in query:
+                if isinstance(query[column], dict):
+                    for op, value in query[column].items():
+                        if op == "$gt":
+                            conditions.append(f'{column} > ?')
+                        elif op == "$gte":
+                            conditions.append(f'{column} >= ?')
+                        elif op == "$lt":
+                            conditions.append(f'{column} < ?')
+                        elif op == "$lte":
+                            conditions.append(f'{column} <= ?')
+                        values.append(value)
+                else:
+                    conditions.append(f'{column} = ?')
+                    values.append(query[column])
+
         query_sql += " AND ".join(conditions)
         query_sql += f" LIMIT {size}"
-        # Execute a consulta SQL
-        cursor.execute(query_sql, values)
-        # Recupere os resultados
-        results = cursor.fetchall()
 
-        if projection:
-            # Verifica a projeção e ajusta a consulta SQL
-            if 'value' in projection and projection['value'] == 1:
-                print()
-                #query_sql = query_sql.replace("*", "value")
-            if '_id' in projection and projection['_id'] == 0:
-                print()
-                #query_sql = query_sql.replace(", _id", "")
+        return query_sql, values
 
-        return results
+    def gerar_query(nome_tabela, ordem=None, limite=1000):
+        # Conecte-se ao banco de dados SQLite
+        conn = sqlite3.connect('seu_banco_de_dados.db')
+        cursor = conn.cursor()
 
+        # Substitua "seu_timestamp_column" pelo nome da coluna de timestamp.
+        base_sql = f"SELECT * FROM {nome_tabela}"
 
+        # Adicione a cláusula de ordenação se a ordem for especificada
+        if ordem is not None:
+            if ordem == 1:
+                base_sql += " ORDER BY seu_timestamp_column ASC"
+            elif ordem == -1:
+                base_sql += " ORDER BY seu_timestamp_column DESC"
+
+        # Adicione a cláusula LIMIT
+        base_sql += f" LIMIT {limite}"
+
+    def convert_data_to_zip(self, lista, tupla):
+        if len(lista) != len(tupla):
+            raise ValueError("A lista e a tupla devem ter o mesmo número de elementos.")
+        resultado = dict(zip(lista, tupla))
+        return resultado
+
+    def insert_dict(self, tabela, dicionario):
+        # Verifique se a tabela existe e, se não existir, crie-a
+        self.cursor.execute(f"PRAGMA table_info({tabela})")
+        if not self.cursor.fetchall():
+            # Crie a tabela com colunas baseadas nas chaves e tipos do dicionário
+            create_table_sql = f"CREATE TABLE {tabela} ({', '.join(f'{chave} {type(valor).__name__}' for chave, valor in dicionario.items())})"
+            self.cursor.execute(create_table_sql)
+        # Insira os dados do dicionário na tabela
+        insert_sql = f"INSERT INTO {tabela} ({', '.join(dicionario.keys())}) VALUES ({', '.join(['?'] * len(dicionario))})"
+        self.cursor.execute(insert_sql, list(dicionario.values()))
+        # Comita a transação e feche a conexão
+        self.conn.commit()
+
+    def list_verifier(self, lista1: list, lista2: list):
+        """
+        Verifica se o conteudo da lista1 existe na lista2
+        :param lista1:
+        :param lista2:
+        :return:
+        """
+        conjunto1 = set(lista1)
+        conjunto2 = set(lista2)
+        return conjunto1.issubset(conjunto2)
+
+    def convert_timestamp(self, obj):
+        if '$date' in obj:
+            date_str = obj['$date'].replace('Z', '+00:00')
+            datetime_obj = datetime.fromisoformat(date_str)
+            timestamp_unix = int(datetime_obj.timestamp())
+            return timestamp_unix
+        else:
+            return obj
+
+    def convert_data_to_string(self, d):
+        for chave, valor in d.items():
+            if isinstance(valor, dict):
+                # Se o valor é um dicionário, converte para JSON
+                d[chave] = json.dumps(valor)
+            elif isinstance(valor, list):
+                # Se o valor é uma lista, verificamos se há dicionários nas listas e os convertemos
+                for i, item in enumerate(valor):
+                    if isinstance(item, dict):
+                        valor[i] = json.dumps(item)
+            elif isinstance(valor, tuple):
+                # Se o valor é uma tupla, verificamos se há dicionários nas tuplas e os convertemos
+                valor_lista = list(valor)
+                for i, item in enumerate(valor_lista):
+                    if isinstance(item, dict):
+                        valor_lista[i] = json.dumps(item)
+                d[chave] = tuple(valor_lista)
+        return d
+
+    def convert_data_to_dict(self, d):
+        for chave, valor in d.items():
+            if isinstance(valor, str):
+                try:
+                    # Tenta analisar o valor como JSON
+                    valor_parseado = json.loads(valor)
+                    if isinstance(valor_parseado, dict):
+                        # Se o valor é um dicionário, substitui o valor original
+                        d[chave] = self.convert_data_to_dict(valor_parseado)
+                    elif isinstance(valor_parseado, list):
+                        # Se o valor é uma lista, verifica se há dicionários nas listas
+                        for i, item in enumerate(valor_parseado):
+                            if isinstance(item, dict):
+                                valor_parseado[i] = self.convert_data_to_dict(item)
+                        d[chave] = valor_parseado
+                except json.JSONDecodeError:
+                    # Se não for um JSON válido, mantém o valor original
+                    pass
+        return d
 
     # ==================================================================================================================
-    def fetch_data(self, table_name, condition=None):
-        """
-        :param table_name: table name in the database
-        :param condition: condition after WHERE for  in the database
-        :return: table with data
-        """
-        condition = f"WHERE {condition}" if condition else ""
-        select_data_sql = f"SELECT * FROM {table_name} {condition}"
-        self.cursor.execute(select_data_sql)
-        return self.cursor.fetchall()
-
     """
-    Auxiliary functions for the operation of SQLITE
+    Sqlite manipulation auxiliary functions
     """
     # ==================================================================================================================
     def create_table(self, table_name, columns):
@@ -250,79 +396,16 @@ class SQLiteDB:
                 table_keys[table_name] = primary_key
         return table_keys
 
-    def list_verifier(self, lista1: list, lista2: list):
+    def fetch_data(self, table_name, condition=None):
         """
-        Verifica se o conteudo da lista1 existe na lista2
-        :param lista1:
-        :param lista2:
-        :return:
+        :param table_name: table name in the database
+        :param condition: condition after WHERE for  in the database
+        :return: table with data
         """
-        conjunto1 = set(lista1)
-        conjunto2 = set(lista2)
-        return conjunto1.issubset(conjunto2)
-
-    def convert_timestamp(self,obj):
-        if '$date' in obj:
-            date_str = obj['$date'].replace('Z', '+00:00')
-            datetime_obj = datetime.fromisoformat(date_str)
-            timestamp_unix = int(datetime_obj.timestamp())
-            return timestamp_unix
-        else:
-            return obj
-
-    def convert_data_to_string(self, d):
-        for chave, valor in d.items():
-            if isinstance(valor, dict):
-                # Se o valor é um dicionário, converte para JSON
-                d[chave] = json.dumps(valor)
-            elif isinstance(valor, list):
-                # Se o valor é uma lista, verificamos se há dicionários nas listas e os convertemos
-                for i, item in enumerate(valor):
-                    if isinstance(item, dict):
-                        valor[i] = json.dumps(item)
-            elif isinstance(valor, tuple):
-                # Se o valor é uma tupla, verificamos se há dicionários nas tuplas e os convertemos
-                valor_lista = list(valor)
-                for i, item in enumerate(valor_lista):
-                    if isinstance(item, dict):
-                        valor_lista[i] = json.dumps(item)
-                d[chave] = tuple(valor_lista)
-
-        return d
-
-    def convert_data_to_dict(self, d):
-        for chave, valor in d.items():
-            if isinstance(valor, str):
-                try:
-                    # Tenta analisar o valor como JSON
-                    valor_parseado = json.loads(valor)
-                    if isinstance(valor_parseado, dict):
-                        # Se o valor é um dicionário, substitui o valor original
-                        d[chave] = self.convert_data_to_dict(valor_parseado)
-                    elif isinstance(valor_parseado, list):
-                        # Se o valor é uma lista, verifica se há dicionários nas listas
-                        for i, item in enumerate(valor_parseado):
-                            if isinstance(item, dict):
-                                valor_parseado[i] = self.convert_data_to_dict(item)
-                        d[chave] = valor_parseado
-                except json.JSONDecodeError:
-                    # Se não for um JSON válido, mantém o valor original
-                    pass
-        return d
-
-    def get_tables(self):
-        """
-        :return: List with table values
-        """
-        # Consulta SQL para obter a lista de tabelas no banco de dados
-        query = f"SELECT name FROM sqlite_master WHERE type='table';"
-        self.cursor.execute(query)
-        # Recupera os resultados da consulta
-        tables = self.cursor.fetchall()
-        # Extrai os nomes das tabelas a partir dos resultados
-        table_names = [table[0] for table in tables]
-
-        return table_names
+        condition = f"WHERE {condition}" if condition else ""
+        select_data_sql = f"SELECT * FROM {table_name} {condition}"
+        self.cursor.execute(select_data_sql)
+        return self.cursor.fetchall()
 
     def close(self):
         self.conn.close()
--- teste3.py
+++ teste3.py
@@ -1,7 +1,44 @@
+import time
+
 from sqldatabase import SQLiteDB
-import json
 
-db = SQLiteDB("seubanco.db")
+db = SQLiteDB("teste.db")
+inicio = time.time()
+# =====================================
 
-print(db.fetch_data("caral"))
-print(db.get_tables_and_columns())
+data1 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e22"},"idShort":"ReactivePower","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.935Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.158Z"},"id":31}
+data2 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e0a"},"idShort":"XVibrationAccelerationDeviation","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.738Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.096Z"},"id":7}
+data3 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e11"},"idShort":"YVibrationAccelerationSkewness","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.797Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.107Z"},"id":14}
+data4 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e1c"},"idShort":"Current","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.885Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.135Z"},"id":25}
+data5 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e12"},"idShort":"YVibrationAccelerationDeviation","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.805Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.108Z"},"id":15}
+data6 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e20"},"idShort":"Frequency","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.92Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.155Z"},"id":29}
+data7 = {"_id":{"$oid":"64c3ee842b13e057bf0d8e23"},"idShort":"ApparentPower","category":"VARIABLE","value":{"value":0,"data_type":6,"source_timestamp":{"$date":"2023-07-28T16:36:17.943Z"},"server_timestamp":None},"valueType":11,"timestamp":{"$date":"2023-07-28T16:36:20.161Z"},"id":32}
+
+start = {"$date":"2023-07-27T16:36:20.158Z"}
+end = {"$date":"2023-07-29T16:36:20.158Z"}
+
+query = {
+                '$and': [
+                    {'timestamp': {'$gte': start}},
+                    {'timestamp': {'$lte': end}},
+                    {'idShort': str("ReactivePower")}
+                ]
+            }
+# data = {"nome":"Andre", "idade": 20, "curso": {'Alfabetizacao': 'Gurilandia', 'Ensino medio': 'Carmo'}, 'timestamp': {'$date': '2023-07-29T16:36:20.158Z'}}
+#
+# db.insert_one(data7)
+#
+# tac = db.get_tables()
+# print(tac)
+# db.drop_table("variable_history")
+
+print(db.count())
+print(db.fetch_data("variable_history"))
+
+print("Dados pesquisados: ")
+print(db.find(query, size=5))
+
+# =====================================
+fim = time.time()
+print(fim-inicio)
+db.close()
